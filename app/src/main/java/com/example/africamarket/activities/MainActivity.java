1d3226311fe74c887324f837ddbdd07fd859720c
Jofiag <Aguemon.wahab.97@gmail.com>
2020-05-14 08:16:39 +0200

    Create the AdminLogin Activity

--- app/src/main/java/com/example/africamarket/activities/MainActivity.java
+++ app/src/main/java/com/example/africamarket/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.africamarket;
+package com.example.africamarket.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.africamarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
