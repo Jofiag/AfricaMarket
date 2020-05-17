d4727a27daedd9434c780bcf332c23f8b18c3ae7
Jofiag <Aguemon.wahab.97@gmail.com>
2020-05-17 16:38:24 +0200

    Create a model for each product.

---
+++ app/src/main/java/com/example/africamarket/model/Product.java
@@ -0,0 +1,56 @@
+package com.example.africamarket.model;
+
+public class Product {
+    private String type;
+    private String name;
+    private String price;
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(String type, String name, String price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String type, String name, String price, String imageUrl) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
